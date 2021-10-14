c17e7e765c2809e5a87766c210f786a84b48f1ef
Bartosz Sekula <Bartosz.Sekula@episerver.com>
2021-10-14 14:56:19 +0200

    Fix path to Alloy

--- setup.cmd
+++ setup.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
 REM Copy the database files to the site.
-ROBOCOPY build/database/ src/Alloy.Mvc.Template/App_Data/ Alloy.mdf DefaultSiteContent.episerverdata /XC /XN /XO
+ROBOCOPY build/database/ src/AlloyTemplates/App_Data/ Alloy.mdf DefaultSiteContent.episerverdata /XC /XN /XO
 
 EXIT /B %ERRORLEVEL%
